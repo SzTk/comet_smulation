8e4a917bdc9f2a90f374003725a9248b285a977c
Takayuki Suzuki <takayuki@darkhaloes.com>
2026-05-29 23:57:13 +0900

    feat: project scaffold with FastAPI and static file serving

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from api.routes import router
+
+app = FastAPI(title="Comet Simulation")
+app.include_router(router)
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
